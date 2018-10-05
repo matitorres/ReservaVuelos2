5728c8b29f20c01d5b11dd394c7aba4c55f75b7d
matitorres <41597953+matitorres@users.noreply.github.com>
2018-10-05 18:40:41 -0300

    TestClaseVuelo con test para todos los métodos

--- src/reservadevuelos/modelo/TestClaseVuelo.java
+++ src/reservadevuelos/modelo/TestClaseVuelo.java
@@ -3,10 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package accesoabasededatos.modelo;
+package reservadevuelos.modelo;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,15 +18,35 @@ import java.util.logging.Logger;
 public class TestClaseVuelo {
     public static void main(String[] args) throws SQLException, ClassNotFoundException{        
         try {
-            Class.forName("org.mariadb.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
             Conexion conexion = new Conexion();
             VueloData vueloData = new VueloData(conexion);
             
-            Vuelo vuelo = new Vuelo("Emirates", "Boeing 747", 1, 2, LocalDate.now(), LocalDate.now(), "n");
-            vueloData.altaVuelo(vuelo);
+            /*Vuelo vuelo = new Vuelo("Aerolineas Argentinas", "Boeing 767", 1, 2, LocalDate.now(), LocalDate.now(), "n");
+            vueloData.altaVuelo(vuelo);*/
+            
+            /*vueloData.bajaVuelo(5);*/
+            
+            /*Vuelo vuelo = new Vuelo(6,"LATAM", "Airbus A320", 1, 5, LocalDate.now(), LocalDate.now(), "n");
+            vueloData.modificarVuelo(vuelo);*/
+            
+            /*Vuelo vuelo = vueloData.buscarVuelo(7);
+            System.out.println("// "+vuelo.getIdVuelo());
+            System.out.println("// "+vuelo.getAerolinea()+" - "+vuelo.getTipoAeronave());
+            System.out.println("// idOrigen:"+vuelo.getIdCiudadOrigen()+" - idDestino:"+vuelo.getIdCiudadDestino());
+            System.out.println("// estado:"+vuelo.getEstado());*/
+            
+            /*List<Vuelo> vuelos = vueloData.obtenerVuelos();
+            for(int i = 0; i < vuelos.size(); i++){
+                System.out.println("======================== "+vuelos.get(i).getIdVuelo()+" ==========================");
+                System.out.println("// "+vuelos.get(i).getAerolinea()+" - "+vuelos.get(i).getTipoAeronave());
+                System.out.println("// idOrigen:"+vuelos.get(i).getIdCiudadOrigen()+" - idDestino:"+vuelos.get(i).getIdCiudadDestino());
+                System.out.println("// estado:"+vuelos.get(i).getEstado());
+                System.out.println("=====================================================");
+            }*/
             
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(accesoabasededatos.modelo.TestClaseVuelo.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(reservadevuelos.modelo.TestClaseVuelo.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
