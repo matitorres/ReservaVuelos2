fc6363049a2aa8cbc4895c7f7e4566ae0a2f9b1b
matitorres <41597953+matitorres@users.noreply.github.com>
2018-10-08 15:10:01 -0300

    Actualizado testeado funcionando

--- src/reservadevuelos/modelo/Asiento.java
+++ src/reservadevuelos/modelo/Asiento.java
@@ -10,86 +10,71 @@ package reservadevuelos.modelo;
  * @author Gabriel
  */
 public class Asiento {
-  //ATRIBUTOS
     private int idAsiento=-1;
     private Vuelo vuelo;
     private String codigoAsiento;
     private boolean disponible;
     private float precio;
     
-  //CONSTRUCTORES
-  public Asiento(){
+    
+    public Asiento(){
     this.idAsiento=-1;
   }
-  
-  public Asiento(int idAsiento, Vuelo vuelo, String codigoAsiento, boolean disponible, float precio){
+    public Asiento(int idAsiento, Vuelo vuelo, String codigoAsiento, float precio, boolean disponible){
     this.idAsiento=idAsiento;
     this.vuelo=vuelo;
     this.codigoAsiento=codigoAsiento;
     this.disponible=disponible;
     this.precio=precio;
   }
-  
-  public Asiento(Vuelo vuelo, String codigoAsiento, boolean disponible, float precio){
+    public Asiento(Vuelo vuelo, String codigoAsiento, float precio, boolean disponible){
     this.idAsiento=-1;
     this.vuelo=vuelo;
     this.codigoAsiento=codigoAsiento;
     this.disponible=disponible;
     this.precio=precio;
   }
-
-  public Asiento(Vuelo vuelo, String codigoAsiento, float precio){
-    this.idVuelo=-1;
+    public Asiento(Vuelo vuelo, String codigoAsiento, float precio){
+    this.idAsiento=-1;
     this.vuelo=vuelo;
     this.codigoAsiento=codigoAsiento;
     this.precio=precio;
   }
-  
-  public Asiento (float precio) {
+    public Asiento (float precio) {
     this.idAsiento = -1;
     this.precio = precio;
     }
-//FIN CONSTRUCTORES
 
+    
     public int getIdAsiento() {
         return idAsiento;
     }
-
     public void setIdAsiento(int idAsiento) {
         this.idAsiento = idAsiento;
     }
-
-    public int getVuelo() {
+    public Vuelo getVuelo() {
         return vuelo;
     }
-
     public void setVuelo(Vuelo vuelo) {
         this.vuelo = vuelo;
     }
-
     public boolean getDisponible() {
         return disponible;
     }
-
     public void setDisponible(boolean disponible) {
         this.disponible = disponible;
     }
-
     public String getCodigoAsiento() {
         return codigoAsiento;
     }
-
     public void setCodigoAsiento(String codigoAsiento) {
         this.codigoAsiento = codigoAsiento;
     }
-
     public float getPrecio() {
         return precio;
     }
-
     public void setPrecio(float precio) {
         this.precio = precio;
     }
-
 
 }
