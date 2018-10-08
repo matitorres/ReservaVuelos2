43fd3f7c3615d4ba9e6df4325c9f47f4335c7a59
matitorres <41597953+matitorres@users.noreply.github.com>
2018-10-08 15:11:05 -0300

    Testeados todos los metodos

--- src/reservadevuelos/modelo/TestClaseAsiento.java
+++ src/reservadevuelos/modelo/TestClaseAsiento.java
@@ -5,118 +5,50 @@
  */
 package reservadevuelos.modelo;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.ParseException;
-import java.time.LocalDate;
-import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.sql.Statement;
-
-import java.util.ArrayList;
 
 /**
  *
- * @author Gabriel
+ * @author asus pc
  */
- 
-
 public class TestClaseAsiento {
-  public static void main(String[] args) throws SQLException  { //throws SQLException                     //argumentos variables del metodo main
-   /* String url="jdbc:mysql://localhost/reservadevuelos";
-    String usuario="root";
-    String password="";*/
-    PreparedStatement ps;
-    
-        
+    public static void main(String[] args) throws SQLException, ClassNotFoundException{        
         try {
-          Class.forName("org.mariadb.jdbc.Driver");
-          //conexion=DriverManager.getConnection(url, usuario, password);
-          
-          
-          Conexion conexion=new Conexion();//Creo la conexion
-          AsientoData asientoData=new AsientoData(conexion);//instancio un ClienteData
-         
-            //Codigo para Testear el ABM de la clase Asiento
-          
-          
-           //Instrucciones para Testear dar de Alta un asiento(guardarAsiento()) 
-           //----------------------------------------------------------- 
-          // Asiento asiento=new Asiento(5,1,1,"P45",2450);//INSERT(sin id) Instancio un Asiento sin id, para insertar
-           //asientoData.guardarAsiento(asiento);
-           //-----------------------------------------------------------           
-
-           
-
-            //Instrucciones para Testear actualizar  
-           //----------------------------------------------------------- 
-           // Asiento asiento=new Asiento(1,5,1,1,"P44",2250);;//UPDATE Instancio una Compra con id para hacer un UPDATE
-           // asientoData.actualizarAsiento(asiento);//UPDATE
-           //----------------------------------------------------------- 
-           
-           
-           
-         
-          //BUSCAR COMPRA
-          //---------------------------------------------- 
-         /* Asiento r=asientoData.buscarAsiento(2);
-          
-            System.out.println(r.getIdAsiento()); 
-            System.out.println(r.getIdVuelo());
-            System.out.println(r.getPasillo());
-            System.out.println(r.getDisponible());
-            System.out.println(r.getUbicacion());
-            System.out.println(r.getPrecio());*/
+            Class.forName("com.mysql.jdbc.Driver");
+            Conexion conexion = new Conexion();
+            AsientoData asientoData = new AsientoData(conexion);
             
+            /*Vuelo vuelo = new Vuelo(11);
+            Asiento asiento = new Asiento(vuelo, "P016", 1500f, true);
+            asientoData.guardarAsiento(asiento);*/
             
-          //---------------------------------------------- 
-          
-          
-          
-          
-          //DELETE Tengo que pasar un id registrado en la tabla
-          //---------------------------------------------- 
-          //asientoData.borrarAsiento(3);
-          //---------------------------------------------- 
-                   
- 
-         // OBTENER Asientos Este metodo todavia no esta testeado
-         //---------------------------------------------- 
-         // asientoData.obtenerAsientos();
-         //---------------------------------------------- 
-          
-          
-        
-          
-       //CONSULTA A BD
-       //---------------------------------------------- 
-       
-      /*   ps= conexion.getConexion().prepareStatement("SELECT * FROM asiento");//
-         ResultSet rs=ps.executeQuery();
-         
-         while(rs.next()){
+            /*asientoData.borrarAsiento(220);*/
             
-       System.out.print("idAsiento: ");System.out.println(rs.getInt(1));
-       System.out.print("idVuelo: ");System.out.println(rs.getInt(2));
-       System.out.print("Pasillo: ");System.out.println(rs.getInt(3));
-       System.out.print("Disponible: ");System.out.println(rs.getInt(4));
-       System.out.print("Ubicacion: ");System.out.println(rs.getString(5)+" ");
-       System.out.print("Precio: $ ");System.out.println(rs.getDouble(6));
-         
-       System.out.println("____________________");       
-         }
-         */
-         // FIN CONSULTA 
-                
+            /*Vuelo vuelo = new Vuelo(13);
+            Asiento asiento = new Asiento(219, vuelo, "P015", 3500f, false);
+            asientoData.actualizarAsiento(asiento);*/
+            
+            /*Asiento asiento = asientoData.buscarAsiento(219);
+            System.out.println("|| "+asiento.getIdAsiento());
+            System.out.println("|| idVuelo: "+asiento.getVuelo().getIdVuelo());
+            System.out.println("|| precio: "+asiento.getPrecio());
+            System.out.println("|| disponible: "+asiento.getDisponible());*/
+            
+            /*List<Asiento> asientos = asientoData.obtenerAsientos();
+            for(int i = 0; i < asientos.size(); i++){
+                System.out.println("");
+                System.out.println("======================== "+asientos.get(i).getIdAsiento()+" ==========================");
+                System.out.println("|| idVuelo: "+asientos.get(i).getVuelo().getIdVuelo());
+                System.out.println("|| precio: "+asientos.get(i).getPrecio());
+                System.out.println("|| disponible: "+asientos.get(i).getDisponible());
+                System.out.println("=====================================================");
+            }*/
+            
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(TestClaseCompra.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(reservadevuelos.modelo.TestClaseVuelo.class.getName()).log(Level.SEVERE, null, ex);
         }
-}
-    
+    }
 }
