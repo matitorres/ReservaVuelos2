bf4fb2cbe9a4f0d82343220cd56d4248e655a0a5
pedroluti1989 <43482609+pedroluti1989@users.noreply.github.com>
2018-10-13 13:35:33 -0300

    Update Conexion.java
    Se modifico la clase Conexion en base a las correciones dadas

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -8,36 +8,51 @@ package conexion;
 
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Conexion{
 
-    private Connection conexion = null;
+      private static Connection con ;
+    private static Conexion conexion = null;
+    
     private String servidor = "localhost";
-    private String database = "reservadevuelos";
+    private String baseDatos = "reservadevuelos";
     private String usuario = "root";
     private String password = "";
-    private String url = "";
-    private String query;
 
-    public Conexion() {
+    private Conexion() {
+
         try {
-
             Class.forName("com.mysql.jdbc.Driver");
-            url = "jdbc:mysql://" + servidor + "/" + database;
-            conexion = DriverManager.getConnection(url, usuario, password);
-            System.out.println("Conexion a Base de Datos " + url + " . . . . .Ok");//MENSAJE QUE EXPRESA Q LA CONEXION FUEN EXITOSA
-
+            String url = "jdbc:mysql://" + servidor + "/" + baseDatos;
+            con = DriverManager.getConnection(url, usuario, password);
+            
         } catch (SQLException ex) {
-            System.out.println(ex);
+               System.out.println("Error al al hacer la conexion");
         } catch (ClassNotFoundException ex) {
-            System.out.println(ex);
+            System.out.println("Error al pasar nombre de driver");
         }
     }
+    
+    
+    
+    // DEVUELVO LA CONEXION Y CONTROLO QUE NO SE HAYA CREADO LA INSTANCIA, QUE SERA LA UNICA 
+    //YA QUE EN CASO DE Q NO SEA NULL, O ESTE CERRADA O NO SEA VALIDA,  YA ESTARA CREADA Y SE RETORNARA
+    
+    public static Connection getConexion(){
+      
+            try {
+                 if(conexion == null || con.isClosed()|| !con.isValid(0)) {
+                    conexion = new Conexion();
+                 }
 
-
-
-    public Connection getConexion() {
-        return conexion;
+            } catch (SQLException ex) {
+                System.out.println("Fallo en getConexion\n");
+            }
+        
+        return con;
     }
+
 
 }
