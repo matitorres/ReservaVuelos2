c860241a73b5141f064fde3718c56638fa685dd6
matitorres <41597953+matitorres@users.noreply.github.com>
2018-10-25 17:02:02 -0300

    probando git

---
+++ AsientoData.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clasesData;
+
+import clases.Asiento;
+import clases.Vuelo;
+import conexion.Conexion;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class AsientoData {    
+     
+    public AsientoData() {}
+
+    public void guardarAsiento(Asiento asiento){
+        try {
+            
+            String sql = "INSERT INTO asiento (idVuelo, ubicacion, precio, disponible) VALUES ( ? , ? , ? , ? );";
+
+            PreparedStatement statement = Conexion.getConexion().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            statement.setInt(1, asiento.getVuelo().getIdVuelo());
+            statement.setString(2, asiento.getCodigoAsiento());
+            statement.setFloat(3, asiento.getPrecio());
+            statement.setBoolean(4, asiento.getDisponible());
+            
+            statement.executeUpdate();
+            
+            ResultSet rs = statement.getGeneratedKeys();
+
+            if (rs.next()) {
+                asiento.setIdAsiento(rs.getInt(1));
+            } else {
+                System.out.println("No se pudo obtener el id luego de insertar el Asiento");
+            }
+            statement.close();
+    
+        } catch (SQLException ex) {
+            System.out.println("Error al insertar un Asiento: " + ex.getMessage());
+        }
+    }   
+    public void borrarAsiento(int id){
+    try {
+            
+            String sql = "DELETE FROM asiento WHERE idAsiento =?;";
+
+            PreparedStatement statement = Conexion.getConexion().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, id);
+                       
+            statement.executeUpdate();
+              //System.out.println(statement.executeUpdate()); Para saber que tipo de valor devuelve 
+                           
+            statement.close();
+    
+        } catch (SQLException ex) {
+            System.out.println("Error al Eliminar una Asiento " + ex.getMessage());
+        }
+        
+    
+    }
+    public Asiento buscarAsiento(int id){
+            Asiento asiento=null;
+            Vuelo vuelo = new Vuelo();
+    try {
+            
+            String sql = "SELECT * FROM asiento WHERE idAsiento =?;";
+
+            PreparedStatement statement = Conexion.getConexion().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, id);
+           
+            
+            ResultSet resultSet=statement.executeQuery();
+            
+            while(resultSet.next()){
+                asiento = new Asiento();
+                asiento.setIdAsiento(resultSet.getInt("idAsiento"));
+                vuelo.setIdVuelo(resultSet.getInt("idVuelo"));
+                asiento.setVuelo(vuelo);
+                asiento.setCodigoAsiento(resultSet.getString("ubicacion"));
+                asiento.setPrecio(resultSet.getFloat("precio"));
+                asiento.setDisponible(resultSet.getBoolean("disponible"));
+             }      
+            statement.close();
+                
+        } catch (SQLException ex) {
+            System.out.println("Error al buscar un Asiento: " + ex.getMessage());
+        }
+        
+        return asiento;
+    }     
+    public void actualizarAsiento(Asiento asiento){
+    
+        try {
+            
+            String sql = "UPDATE asiento SET idVuelo = ? , ubicacion =?, precio=?, disponible=? WHERE idAsiento = ?;";
+
+            PreparedStatement statement = Conexion.getConexion().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            statement.setInt(1, asiento.getVuelo().getIdVuelo());
+            statement.setString(2, asiento.getCodigoAsiento());
+            statement.setFloat(3, asiento.getPrecio());
+            statement.setBoolean(4, asiento.getDisponible());
+            statement.setInt(5, asiento.getIdAsiento());
+            statement.executeUpdate();
+                    
+            statement.close();
+    
+        } catch (SQLException ex) {
+            System.out.println("Error ACTUALIZAR el Asiento : " + ex.getMessage());
+        }
+    
+    }
+    public List<Asiento> obtenerAsientos(){
+        List<Asiento> asientos = new ArrayList<Asiento>();
+            
+
+        try {
+            String sql = "SELECT * FROM asiento;";
+            PreparedStatement statement = Conexion.getConexion().prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+            Asiento asiento;
+            while(resultSet.next()){
+                asiento = new Asiento();
+                Vuelo vuelo = new Vuelo();
+                asiento.setIdAsiento(resultSet.getInt("idAsiento"));
+                vuelo.setIdVuelo(resultSet.getInt("idVuelo"));
+                asiento.setVuelo(vuelo);
+                asiento.setCodigoAsiento(resultSet.getString("ubicacion"));
+                asiento.setPrecio(resultSet.getFloat("precio"));
+                asiento.setDisponible(resultSet.getBoolean("disponible"));
+                
+                asientos.add(asiento);
+            }      
+            statement.close();
+        } catch (SQLException ex) {
+            System.out.println("Error al obtener los Asientos: " + ex.getMessage());
+        }
+        
+        
+        return asientos;
+    }     
+        
+}
---
+++ VueloData.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clasesData;
+
+import clases.Asiento;
+import clases.Ciudad;
+import clases.Vuelo;
+import conexion.Conexion;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author asus pc
+ */
+public class VueloData {
+    
+    public VueloData(){}
+        
+    public void altaVuelo(Vuelo vuelo, Asiento asiento){
+        try {
+            // Crea nuevo vuelo
+            String sql = "INSERT INTO vuelo (aerolinea, tipoAeronave, idCiudadOrigen, idCiudadDestino, fechaSalida, fechaArribo, estado) VALUES ( ? , ? , ? , ? , ? , ? , ? );";
+            PreparedStatement statement = Conexion.getConexion().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, vuelo.getAerolinea());
+            statement.setString(2, vuelo.getTipoAeronave());
+            statement.setInt(3, vuelo.getCiudadOrigen().getIdCiudad());
+            statement.setInt(4, vuelo.getCiudadDestino().getIdCiudad());
+            statement.setDate(5, Date.valueOf(vuelo.getFechaSalida()));
+            statement.setDate(6, Date.valueOf(vuelo.getFechaArribo()));
+            statement.setString(7, vuelo.getEstado());
+            statement.executeUpdate();
+            
+            ResultSet rs = statement.getGeneratedKeys();
+            if (rs.next()) {
+                vuelo.setIdVuelo(rs.getInt(1));
+            } else {
+                System.out.println("No se pudo obtener el id luego de insertar un vuelo");
+            }
+            
+            // Crea los asientos para ese vuelo
+            vuelo.setAsientos(asiento);
+            
+            String sql2 = "INSERT INTO asiento (idVuelo, ubicacion, precio,"
+                    + "disponible) VALUES ( ? , ? , ? , ? );";
+            
+            PreparedStatement statement2 = Conexion.getConexion().prepareStatement(sql2, Statement.RETURN_GENERATED_KEYS);
+            for(int i=0;i<vuelo.getAsientos().size();i++) {
+                statement2.setInt(1, rs.getInt(1));
+                statement2.setString(2, vuelo.getAsientos().get(i).getCodigoAsiento());
+                statement2.setFloat(3, vuelo.getAsientos().get(i).getPrecio());
+                statement2.setBoolean(4, true);
+                statement2.executeUpdate();
+            }
+            
+            statement.close();
+            statement2.close();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al insertar vuelo: " + ex.getMessage());
+        }
+    }
+    public void bajaVuelo(int id){
+        try {
+            String sql = "DELETE FROM vuelo WHERE idVuelo = ? ;";
+            PreparedStatement statement = Conexion.getConexion().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, id);
+            statement.executeUpdate();
+            statement.close();
+        } catch (SQLException ex) {
+            System.out.println("Error al eliminar vuelo: " + ex.getMessage());
+        }
+    }
+    public void modificarVuelo(Vuelo vuelo){
+        try {
+            String sql = "UPDATE vuelo SET aerolinea = '"+vuelo.getAerolinea()+
+                    "', tipoAeronave = '"+vuelo.getTipoAeronave()+
+                    "', idCiudadOrigen = '"+vuelo.getCiudadOrigen().getIdCiudad()+
+                    "', idCiudadDestino = '"+vuelo.getCiudadDestino().getIdCiudad()+
+                    "', fechaSalida = '"+vuelo.getFechaSalida()+
+                    "', fechaArribo = '"+vuelo.getFechaArribo()+
+                    "', estado = '"+vuelo.getEstado()+
+                    "' WHERE idVuelo = '"+vuelo.getIdVuelo()+"';";
+            PreparedStatement statement = Conexion.getConexion().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.executeUpdate();
+            statement.close();
+        } catch (SQLException ex) {
+            System.out.println("Error al modificar vuelo: " + ex.getMessage());
+        }
+    }
+    public Vuelo buscarVuelo(int id){
+        Vuelo vuelo = new Vuelo();
+        Ciudad ciudadOrigen = new Ciudad();
+        Ciudad ciudadDestino = new Ciudad();
+        try {
+            String sql = "SELECT * FROM vuelo WHERE idVuelo = "+id+";";
+            PreparedStatement statement = Conexion.getConexion().prepareStatement(sql);
+            ResultSet rs = statement.executeQuery();
+            while(rs.next()) {
+                vuelo.setIdVuelo(rs.getInt("idVuelo"));
+                vuelo.setAerolinea(rs.getString("aerolinea"));
+                vuelo.setTipoAeronave(rs.getString("tipoAeronave"));
+                ciudadOrigen.setIdCiudad(rs.getInt("idCiudadOrigen"));
+                vuelo.setCiudadOrigen(ciudadOrigen);
+                ciudadDestino.setIdCiudad(rs.getInt("idCiudadDestino"));
+                vuelo.setCiudadDestino(ciudadDestino);
+                vuelo.setFechaSalida(rs.getDate("fechaSalida").toLocalDate());
+                vuelo.setFechaArribo(rs.getDate("fechaArribo").toLocalDate());
+                vuelo.setEstado(rs.getString("estado"));
+            }
+            statement.close();
+        } catch (SQLException ex) {
+            System.out.println("Error al buscar vuelo: " + ex.getMessage());
+        }
+        return vuelo;
+    }
+    public List<Vuelo> obtenerVuelos(){
+        List<Vuelo> vuelos = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM vuelo;";
+            PreparedStatement statement = Conexion.getConexion().prepareStatement(sql);
+            ResultSet rs = statement.executeQuery();
+            Vuelo vuelo;
+            while(rs.next()){
+                vuelo = new Vuelo();
+                Ciudad ciudadOrigen = new Ciudad();
+                Ciudad ciudadDestino = new Ciudad();
+                vuelo.setIdVuelo(rs.getInt("idVuelo"));
+                vuelo.setAerolinea(rs.getString("aerolinea"));
+                vuelo.setTipoAeronave(rs.getString("tipoAeronave"));
+                ciudadOrigen.setIdCiudad(rs.getInt("idCiudadOrigen"));
+                vuelo.setCiudadOrigen(ciudadOrigen);
+                ciudadDestino.setIdCiudad(rs.getInt("idCiudadDestino"));
+                vuelo.setCiudadDestino(ciudadDestino);
+                vuelo.setFechaSalida(rs.getDate("fechaSalida").toLocalDate());
+                vuelo.setFechaArribo(rs.getDate("fechaArribo").toLocalDate());
+                vuelo.setEstado(rs.getString("estado"));
+                vuelos.add(vuelo);
+            }
+            statement.close();
+        } catch (SQLException ex) {
+            System.out.println("Error al obtener vuelos: " + ex.getMessage());
+        }
+        return vuelos;
+    }
+}
